fe533cbffd83708fbfdf999c5b6c022b5c01fd84
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-07-10 13:24:54 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+import ListGroup from "./components/ListGroup";
+
+function app(){
+  return <div><ListGroup /></div>
+}
+
+export default app
---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+//pascalcasing: starting function name with a capital letter
+function Message(){
+
+    const name = 'eugy'
+
+    if (name)
+     return <h1>hello {name}</h1>
+
+    return <h1>hello world</h1>
+}
+
+export default Message 
