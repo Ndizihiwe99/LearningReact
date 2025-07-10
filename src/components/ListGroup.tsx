7ae91019d284601fae4c7ffa2c87b78155d056cc
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-07-10 17:57:21 +0200

    renamed

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
-function ListGroup() {
+//import { MouseEvent } from "react";
 
-  let items = ['Kigali', 'Gasabo', 'Kicukiro', 'Nyarugenge', 'North', 'East', 'West']
-  items = []
+function ListGroup() {
+ 
+  const items = ['Kigali', 'Gasabo', 'Kicukiro', 'Nyarugenge', 'North', 'East', 'West']
+  //items = []
+
+  //const eventClick = {(event: MouseEvent) => console.log(event)}
 
   return (
     <>
     <h1>List</h1>
 
-    {items.length === 0 && <p>no elements found</p>}
-    <ul className= 'ListGroup'>
-      {items.map((item) => <li key={item}>{item}</li>)}
+    {/*items.length === 0 && <p>no elements found</p>*/}
+    <ul className="list-group">
+      {items.map((item, index) =>(<li className="list-group-item" key={item} onClick={()=> console.log(item, index)}>{[item ,index]}</li>) )}
     </ul>
     
     </>
